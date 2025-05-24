bd94a2b2374da8f0a1e718a2ef17c2fcde3f4d1c
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-05-24 22:13:11 +0600

    5.4 Define Model & Fields & Relationship

--- task-management/tasks/models.py
+++ task-management/tasks/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+# ---> Create your models here.
+# class Example(models.Model):
+#     name = models.CharField(max_length=100)
+#     description = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+# -----> Relations :
+# --> One-to-One
+# --> Many-to-One
+# --> Many-to-Many
