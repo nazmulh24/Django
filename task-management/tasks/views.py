fda38ac9861819257afac30fa6c18ac8c01daaa7
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-05-25 20:22:16 +0600

    6.3 Create Task into Database Using Form Data

--- task-management/tasks/views.py
+++ task-management/tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm
-from tasks.models import Employee
+from tasks.models import Employee, Task
 
 
 # --> Create your views here.
@@ -24,9 +24,29 @@ def user_dashboard(request):
 
 
 def create_task(request):
-    # return render(request, "task_form.html")
-
     employees = Employee.objects.all()
     form = TaskForm(employees=employees)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, employees=employees)
+        # print(form)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            data = form.cleaned_data
+            title = data.get("title")
+            description = data.get("description")
+            due_date = data.get("due_date")
+            assigned_to = data.get("assigned_to")
+
+            task = Task.objects.create(
+                title=title, description=description, due_date=due_date
+            )
+
+            for emp_id in assigned_to:
+                employee = Employee.objects.get(id=emp_id)
+                task.assigned_to.add(employee)  # --> Many-to-Many related
+
+            return HttpResponse("Task added successfully!!")
+
     context = {"form": form}
     return render(request, "task_form.html", context)
